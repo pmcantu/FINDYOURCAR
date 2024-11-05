9d0315b04f3ac40271a5c997eefab5f2f1a0d91e
pmcantu <139713090+pmcantu@users.noreply.github.com>
2024-11-04 21:56:52 -0500

    fifth commit

--- app.js
+++ app.js
@@ -1,7 +1,18 @@
+const inputElement = document.getElementById("userInput")
+const outputElement = document.getElementById("output")
 
 function displayInput() {
-    const inputElement = document.getElementById("userInput");
+    
     const inputValue = inputElement.value;
-    console.log(inputvalue)
-    document.getElementById("output").textContent = `You typed : ${inputvalue}`;
+   
+    outputElement.innerText = inputValue
+    
 }
+
+// const userInput = document.querySelector("display_typeInfo")
+// userInput.map(userInput => `<div class="display_typeInfo" id="output">
+    // <h3 class="s_results">Search Results: "${displayInput()}"</h3></div>`
+
+
+inputElement.addEventListener("input", displayInput);
+
