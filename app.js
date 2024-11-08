25cdf1e0148c3e62f2f5b2cf9d2896cb10e92902
pmcantu <139713090+pmcantu@users.noreply.github.com>
2024-11-07 22:01:15 -0500

    11/7/24

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const inputElement = document.getElementById("userInput")
 const outputElement = document.getElementById("output")
+const resultList = document.querySelector(".movie_data")
 
 function displayInput() {
     
@@ -13,6 +14,20 @@ inputElement.addEventListener("input", displayInput);
 
 // fetch and    api
 
-//  fetch('http://www.omdbapi.com/?i=t3896198&apikey=d32854a2&s=fast')
-// .then(res => res.json())
-// .then(data => console.log(data))
+
+// 
+ async function main() {
+    const title = inputElement.value
+const data = await fetch(`https://www.omdbapi.com/?i=t3896198&apikey=d32854a2&s=${title || ""}`)
+const movieData = await data.json()
+const result = movieData.Search
+
+resultList.innerHTML = result.map((movie) => 
+(`<div class = "movie_data-container">
+    <img class = "movie" src = "${movie.Poster}">
+<h2 class = "title">${movie.Title}</h2>
+<h4 class = "year">${movie.Year}</h4>
+</div>`))
+.join("")
+ }
+ 
